59ccd4c8b53b1cc9b606e0d869ad6cacbc7d09e7
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-10-07 10:51:07 +0200

    Update App.js
    This file contends the page connexion to the bank account

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ButtonToolbar } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={running:false};
+    this.connexion = this.connexion.bind(this);
+    this.inscription = this.inscription.bind(this);
+  }
+  
+  connexion(){
+    this.setState({ running: true });
+    console.log("Vous aviez appuyé sur le bouton connexion !");
+  }
+
+  inscription(){
+    console.log("Vous aviez appuyé sur le bouton inscription!");
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <form>
+                <h3>Identifiant</h3>
+                <input type="text" name="id_user"/>
+                <h3>Mot de Passe</h3>
+                <input type="password" name="password" />
+                <br/><br/>
+               
+                <ButtonToolbar>
+               <Button type="submit" color="primary" onClick={this.connexion} >Connexion</Button>
+               <Button color="primary" onClick={this.inscription}>S'inscrire</Button>
+               </ButtonToolbar>
+               <br/>
+               <h6>Mot de passe oublié</h6>
+          </form>
+        </header>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
