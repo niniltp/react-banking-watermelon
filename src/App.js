74362c164780c0a901f1dfe12833a57d7fae4286
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-10-16 13:25:45 +0200

    update App.js Loic
    First page showing the form to sign in/up to its bank account
    The page setting is very poor, need to do it later at the end of the project
    Basic code with React, not too complex now

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, ButtonToolbar } from 'reactstrap';
-
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import users from './data/users';
+import Form from './Form/Form.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={running:false};
-    this.connexion = this.connexion.bind(this);
-    this.inscription = this.inscription.bind(this);
+    this.state={running:false,
+                email : '',
+                password : ''
+    };
   }
   
-  connexion(){
-    this.setState({ running: true });
-    console.log("Vous aviez appuyé sur le bouton connexion !");
-  }
-
-  inscription(){
-    console.log("Vous aviez appuyé sur le bouton inscription!");
-  }
+  
 
   render(){
     return(
       <div className="App">
         <header className="App-header">
-          <form>
-                <h3>Identifiant</h3>
-                <input type="text" name="id_user"/>
-                <h3>Mot de Passe</h3>
-                <input type="password" name="password" />
-                <br/><br/>
-               
-                <ButtonToolbar>
-               <Button type="submit" color="primary" onClick={this.connexion} >Connexion</Button>
-               <Button color="primary" onClick={this.inscription}>S'inscrire</Button>
-               </ButtonToolbar>
-               <br/>
-               <h6>Mot de passe oublié</h6>
-          </form>
+          <Form />
         </header>
       </div>
     );
@@ -48,3 +30,4 @@ class App extends Component {
 }
 
 export default App;
+
