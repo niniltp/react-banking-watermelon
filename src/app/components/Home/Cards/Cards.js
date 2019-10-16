fee1893f87f7705772d589ac788873ecd7508112
niniltp <nhienphan923@gmail.com>
2019-10-16 16:57:21 +0200

    button add card

--- src/app/components/Home/Cards/Cards.js
+++ src/app/components/Home/Cards/Cards.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 import Card from './Card.js';
 import {getCardsByUserId} from "../../../backend/cards_backend";
 import './Cards.css';
@@ -9,32 +10,34 @@ class Cards extends Component {
 
         this.state = {
             isFetching: false,
+            isAddingCard: false,
             cards: []
         }
+
+        this.addCard = this.addCard.bind(this);
     }
 
     componentDidMount() {
+        this.setState({ isFetching: true});
+        this.setState({ cards: getCardsByUserId(this.props.user_id) });
+        this.setState({ isFetching: false})
+    }
 
-        this.setState({
-            isFetching: true
-        });
-
-        this.setState({
-            cards: getCardsByUserId(this.props.user_id)
-        });
-
-        this.setState({
-            isFetching: false
-        })
+    addCard() {
+        this.setState({ isAddingCard: true});
+        console.log("test");
     }
 
     render() {
         return (
-            <div id="creditCards">
+            <div id="creditCardsContainer">
                 <h3>Cards</h3>
-                {this.state.isFetching ? <p>Fetching data...</p> : this.state.cards.map((card, index) => (
-                    <Card key={index} card={card}/>
-                ))}
+                <div id="creditCardsList">
+                    {this.state.isFetching ? <p>Fetching data...</p> : this.state.cards.map((card, index) => (
+                        <Card key={index} card={card}/>
+                    ))}
+                </div>
+                {this.state.isAddingCard ? <p>hig</p>:  <Button outline onClick={this.addCard}>+</Button>}
             </div>
         );
     }
