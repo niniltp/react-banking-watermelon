251e85c1d7513e21c45f4e6003945215d30c1dc1
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-10-22 11:19:26 +0200

    LocalStorage updated
    LocalStorage works now and we can extract the database later

--- src/App.js
+++ src/App.js
@@ -2,33 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import cards from './data/cards';
-import payins from './data/payins';
-import payouts from './data/payouts';
-import transfers from './data/transfers';
-import users from './data/users';
-import wallets from './data/wallets';
+import cards from './database/cards';
+import payins from './database/payins';
+import payouts from './database/payouts';
+import transfers from './database/transfers';
+import users from './database/users';
+import wallets from './database/wallets';
 import SignIn from './Form/SignIn.js';
 import SignUp from './Form/SignUp.js';
 import PwdForgot from './Form/PwdForgot.js';
+
+//Code written by CHEONG Loïc
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      running:false,
       email : '',
-      password : '' ,
-      card : [],
-      payin : [],
-      payout : [],
-      transfer : [],
-      user : [],
-      wallet : []
+      password : ''
     };
   }
+
+
   componentDidMount(){
-    //this fonction fires after the initial render  
+    //this function fires after the initial render  
     //Set the database in localStorage
     localStorage.clear();
     localStorage.setItem("cards", JSON.stringify(cards));
@@ -37,34 +34,8 @@ class App extends Component {
     localStorage.setItem("transfers",JSON.stringify(transfers));
     localStorage.setItem("users",JSON.stringify(users));
     localStorage.setItem("wallets",JSON.stringify(wallets));
-    
-    
-    const A  = JSON.parse(localStorage.getItem("cards"));/*
-    const B  = localStorage.getItem('payins');
-    const payout  = localStorage.getItem("payouts");
-    const transfer  = localStorage.getItem("transfers");
-    const user  = localStorage.getItem("users");
-    const wallet  = localStorage.getItem("wallets");*/
-
-    console.log(this.state.card);
-    this.setState({
-      card : this.state.card.concat(A)/*,
-      payin : this.state.payin.concat(JSON.parse(localStorage.getItem("payins"))),
-      payout : this.state.payout.concat(JSON.parse(localStorage.getItem("payouts"))),
-      transfer : this.state.transfer.concat(JSON.parse(localStorage.getItem("transfers"))),
-      user : this.state.user.concat(this.state.card.concat(JSON.parse(localStorage.getItem("users"))),
-      wallet : this.state.wallet.concat(JSON.parse(localStorage.getItem("wallets")))*/
-    });
-
-    console.log(this.state.card);
-    console.log(A);
-    console.log("hello");
-    console.log(Array.isArray(this.state.card));
-    console.log(Array.isArray(A));
-/*
-    const people = JSON.parse(localStorage.getItem("cards"));
-    console.log(people);
-    console.log(people[0]);*/
+    localStorage.setItem("isAuth",false);
+    localStorage.setItem("UserID",null);
   }
 
   render(){
@@ -78,15 +49,10 @@ class App extends Component {
               <Route path="/pwdForgot" component={PwdForgot}></Route> 
               </Switch>
           </BrowserRouter>
-          
-          
         </header>
-        
       </div>
     );
-
   }
-
 }
 
 export default App;
