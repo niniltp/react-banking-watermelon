19a273bc3214d6edda45d4851468983e5b9b9a98
niniltp <nhienphan923@gmail.com>
2019-10-23 00:41:19 +0200

    update img and logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './app/img/logo.svg';
 import {Route, Link} from "react-router-dom";
 import './App.css';
 import Home from './app/components/Home/Home.js'
