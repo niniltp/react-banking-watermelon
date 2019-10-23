75aff79c986be0b5f2bc3b63cb5fe735387cfb82
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-10-23 10:22:47 +0200

    Add files via upload
    test

---
+++ src/Form/Account.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Account extends Component {
+    
+
+    render(){
+        return(
+          <div className="App">
+              <h1>Hello World !</h1>
+          </div>
+        );
+    
+      }
+
+}
+
+export default Account;
