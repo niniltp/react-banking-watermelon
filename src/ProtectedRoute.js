8cbcde6e6d855971b4da7e9ca5e53edfcc06b3d7
CheongLoic <47603928+CheongLoic@users.noreply.github.com>
2019-10-23 10:24:03 +0200

    Add files via upload
    protected route if the user is not connected

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Route} from 'react-router-dom';
+
+export const ProtectedRoute = ({component : Component, ...rest}) =>{
+    return(
+        <Route
+            {...rest}
+            render = {
+                props => { 
+                    if (JSON.parse(localStorage.getItem("isAuth")) === true){
+                        return <Component {...props}></Component>;
+                    }
+                    else return "ERROR 404 PAGE NOT FOUND";
+                }
+            }
+        />
+    );
+
+};
