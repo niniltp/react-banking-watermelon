3ec84185a86df37324413ecb235bff6121b2612f
niniltp <nhienphan923@gmail.com>
2019-10-23 12:20:53 +0200

    fix repo

--- database/payins.js
+++
@@ -1,9 +0,0 @@
-const payins = [
-    {
-        id: 0,
-        wallet_id: 2,
-        amount: 15000
-    }
-];
-
-export default payins;
--- database/payouts.js
+++
@@ -1,9 +0,0 @@
-const payouts = [
-    {
-        id: 0,
-        wallet_id: 1,
-        amount: 2000
-    }
-];
-
-export default payouts;
