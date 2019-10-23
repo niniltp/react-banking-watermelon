d67ec098604f667f49684dee9447749af82bd322
niniltp <nhienphan923@gmail.com>
2019-10-23 15:40:37 +0200

    amount input

--- src/app/components/Home/fundsMng/Withdraw.js
+++ src/app/components/Home/fundsMng/Withdraw.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {getUserIDAuth} from "../../../services/authenticationManager";
 import {getCardsByUserId} from "../../../backend/cards_backend";
 import CardToSelect from "../Cards/CardToSelect";
+import {FormGroup, Input, Label} from "reactstrap";
+import './fundsMngForm.css';
 
 class Withdraw extends Component {
     constructor(props) {
@@ -10,8 +12,9 @@ class Withdraw extends Component {
         this.state = {
             userID: getUserIDAuth(),
             isFetching: true,
-            selectedCardIndex: null,
-            cards: []
+            cards: [],
+            amount: 0,
+            selectedCardIndex: null
         };
     }
 
@@ -25,6 +28,15 @@ class Withdraw extends Component {
         this.setState({cards: getCardsByUserId(this.state.userID)});
     };
 
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+
+        this.setState({
+            amount: value
+        })
+    };
+
     handleSelect = (index) => {
         this.setState({
             selectedCardIndex: index
@@ -34,6 +46,15 @@ class Withdraw extends Component {
     render = () => {
         return (
             <div className="container-in">
+                <div className={"fundsMng-title"}>
+                    <h3>How much do you want to withdraw from your wallet ?</h3>
+                </div>
+                <FormGroup row className={"fundsMng-formGroup"}>
+                    <Input type="number" min="0" max="999999999999" id="amount" className="creditCardForm-input amount-input"
+                           name="amount" value={this.state.amount} onChange={this.handleChange}/>
+                    <Label for="amount" className="amount-label">₩M</Label>
+                </FormGroup>
+
                 <div id="creditCardsContainer">
                     <h3>Cards</h3>
                     <div id="creditCardsList">
