58de97592d2eea99eb31ec31de2156fba6261978
niniltp <nhienphan923@gmail.com>
2019-10-24 09:45:53 +0200

    fix link logo

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <Link exact to="/"><img src={logo} className="App-logo" alt="logo"/></Link>
+                    <Link to="/"><img src={logo} className="App-logo" alt="logo"/></Link>
                 </header>
                 <Switch>
                     <Route exact path="/" component={SignIn}/>
