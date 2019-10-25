07418ecbcc0f0dd13c453cd36b848af967ff532a
niniltp <nhienphan923@gmail.com>
2019-10-25 16:46:16 +0200

    refactor getUserByID

--- src/app/components/Home/Settings/Settings.js
+++ src/app/components/Home/Settings/Settings.js
@@ -1,51 +1,51 @@
 import React, {Component} from 'react';
 import {getUserIDAuth} from "../../../services/authenticationManager";
-import {getUserById} from "../../../backend/users_backend";
 import Parameters from "./Parameter";
 import {Button} from "reactstrap";
-import {Link, Prompt} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {getUserByID} from "../../../backend/users_backend";
 
 //code written by CHEONG Loïc
 
 class Settings extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-            this.state = ({
-            parameters : []
+        this.state = ({
+            parameters: []
         });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         //to delete at the end or maybe not ...
         console.log("You are in settings !");
         this.initParam();
     }
 
-    addParam(elt,value,msgError,isChanging){
-        this.setState((prevState) => ({ parameters : [...prevState.parameters, {name : elt, val : value}]}));
+    addParam(elt, value, msgError, isChanging) {
+        this.setState((prevState) => ({parameters: [...prevState.parameters, {name: elt, val: value}]}));
     }
-    
-    initParam(){
+
+    initParam() {
         /*This function initializes the array parameters
         with the user's data*/
         let array = [];
-        const user = getUserById(getUserIDAuth()); //it's an Object
+        const user = getUserByID(getUserIDAuth()); //it's an Object
         array.push(user);
-        this.addParam("First name",array[0].first_name);
-        this.addParam("Last name",array[0].last_name);
-        this.addParam("Email",array[0].email);
-        this.addParam("Password",array[0].password);  
+        this.addParam("First name", array[0].first_name);
+        this.addParam("Last name", array[0].last_name);
+        this.addParam("Email", array[0].email);
+        this.addParam("Password", array[0].password);
         //console.log(this.state.parameters);
     }
-    
 
-    render(){
+
+    render() {
         return (
             <div className="container-in">
                 <br/>
                 <h3>Profile</h3>
-                {this.state.parameters.map((param,index)=>(<Parameters key={index} index={index} param={param} />))}
+                {this.state.parameters.map((param, index) => (<Parameters key={index} index={index} param={param}/>))}
                 <br/>
                 <br/>
                 <Link to="/account"><Button color="primary">Go back</Button></Link>
