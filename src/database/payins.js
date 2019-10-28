e632f6a6b092627d736c0938b0dc734b9bd2820c
niniltp <nhienphan923@gmail.com>
2019-10-28 09:40:32 +0100

    new payins

--- src/database/payins.js
+++ src/database/payins.js
@@ -3,6 +3,21 @@ const payins = [
         id: 0,
         wallet_id: 2,
         amount: 15000
+    },
+    {
+        id: 1,
+        wallet_id: 0,
+        amount: 25913
+    },
+    {
+        id: 2,
+        wallet_id: 1,
+        amount: 7199
+    },
+    {
+        id: 3,
+        wallet_id: 2,
+        amount: 35540
     }
 ];
 
